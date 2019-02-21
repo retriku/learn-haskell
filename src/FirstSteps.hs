8fa46eec245adf8d57519d18fcd378de9c05eb99
Renārs Kudiņš <renars.kudins@gmail.com>
2019-02-21 15:16:32 +0200

    tree

--- src/FirstSteps.hs
+++ src/FirstSteps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveFoldable #-}
 module FirstSteps where
 
 import qualified Data.Text.IO as T
@@ -52,9 +53,18 @@ listFoldr f z (MyCons e tail) =
   f e $ listFoldr f z tail
 
 
-data BinaryTree a = Leaf a | Branch (BinaryTree a) a (BinaryTree a)
+data BinaryTree a = Leaf a | Branch (BinaryTree a) a (BinaryTree a) deriving Foldable
 
 elements :: BinaryTree a -> [a]
 elements (Leaf a) = [a]
 elements (Branch t1 e t2) =
   (elements t1) ++ [e] ++ (elements t2)
+
+tree = Branch (Branch (Leaf 1) 2 (Leaf 3)) 4 (Branch (Leaf 5) 6 (Leaf 7))
+
+treeFold :: (a -> b -> b) -> b -> BinaryTree a -> b
+treeFold f z (Leaf e) = f e z
+treeFold f z (Branch t1 e t2) =
+  treeFold f (f e leftFold) t1
+  where
+    leftFold = treeFold f z t2
