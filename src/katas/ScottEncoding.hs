8bbeb6c6904f68ce565c47f81ca5d3d50c56b421
Renārs Kudiņš <renars.kudins@gmail.com>
2019-02-22 16:54:32 +0200

    implemented constructors/desctructors

--- src/katas/ScottEncoding.hs
+++ src/katas/ScottEncoding.hs
@@ -6,27 +6,31 @@ import Prelude hiding (null, length, map, foldl, foldr, take, fst, snd, curry, u
 newtype SMaybe a = SMaybe { runMaybe :: forall b. b -> (a -> b) -> b }
 newtype SList a = SList { runList :: forall b. b -> (a -> SList a -> b) -> b }
 newtype SEither a b = SEither { runEither :: forall c. (a -> c) -> (b -> c) -> c }
-newtype SPair a b = SPair { runPair :: forall c. (a -> b -> c) -> c }
+newtype SPair a b = SPair {
+  runPair :: forall c. (a -> b -> c) -> c
+  -- runPair :: SPair a b -> (a -> b -> c) -> c
+}
 
 toPair :: SPair a b -> (a,b)
-toPair = error "toPair"
+toPair (SPair p) = p(\a b -> (a, b))
 fromPair :: (a,b) -> SPair a b
-fromPair = error "fromPair"
+fromPair (a,b) = SPair(\p -> p a b)
 fst :: SPair a b -> a
-fst = error "fst"
+fst (SPair p) = p(\a _ -> a) 
 snd :: SPair a b -> b
-snd = error "snd"
+snd (SPair p) = p(\_ b -> b)
 swap :: SPair a b -> SPair b a
-swap = error "swap"
+swap (SPair p) = p(\a b -> SPair (\p -> p b a))
 curry :: (SPair a b -> c) -> (a -> b -> c)
 curry = error "curry"
 uncurry :: (a -> b -> c) -> (SPair a b -> c)
 uncurry = error "uncurry"
 
 toMaybe :: SMaybe a -> Maybe a
-toMaybe = error "toMaybe"
+toMaybe (SMaybe m) = m Nothing (\a -> Just a)
 fromMaybe :: Maybe a -> SMaybe a
-fromMaybe = error "fromMaybe"
+fromMaybe Nothing = SMaybe(\e _ -> e)
+fromMaybe (Just v) = SMaybe(\_ f -> f v) 
 isJust :: SMaybe a -> Bool
 isJust = error "isJust"
 isNothing :: SMaybe a -> Bool
@@ -35,9 +39,10 @@ catMaybes :: SList (SMaybe a) -> SList a
 catMaybes = error "catMaybes"
 
 toEither :: SEither a b -> Either a b
-toEither = error "toEither"
+toEither (SEither e) = e Left Right
 fromEither :: Either a b -> SEither a b
-fromEither = error "fromEither"
+fromEither (Left v) = SEither(\l _ -> l v)
+fromEither (Right v) = SEither(\_ r -> r v)
 isLeft :: SEither a b -> Bool
 isLeft = error "isLeft"
 isRight :: SEither a b -> Bool
@@ -46,11 +51,12 @@ partition :: SList (SEither a b) -> SPair (SList a) (SList b)
 partition = error "partition"
 
 toList :: SList a -> [a]
-toList = error "toList"
+toList (SList l) = l [] (\x xs -> x : (toList xs))
 fromList :: [a] -> SList a
-fromList = error "fromList"
+fromList [] = SList(\ni _ -> ni)
+fromList (x:xs) = SList(\_ co -> co x (fromList xs))
 cons :: a -> SList a -> SList a
-cons = error "cons"
+cons x xs = SList (\_ co -> co x xs)
 concat :: SList a -> SList a -> SList a
 concat = error "concat"
 null :: SList a -> Bool
