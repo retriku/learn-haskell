f335ae3dfcb48863d9590ea3250786752999caa1
Renārs Kudiņš <renars.kudins@gmail.com>
2019-03-01 15:52:57 +0200

    PolyvariadicFunctions

---
+++ src/katas/PolyvariadicFunctions.hs
@@ -0,0 +1,13 @@
+module PolyvariadicFunctions where
+
+-- `polyAdd` sums its arguments, all `Int`s.
+polyAdd :: ???
+polyAdd = error "TODO: polyAdd"
+
+-- `polyList` turns its arguments into a list, polymorphically.
+polyList :: ???
+polyList = error "TODO: polyList"
+
+-- `polyWords` turns its arguments into a spaced string.
+polyWords :: ???
+polyWords = error "TODO: polyList"
