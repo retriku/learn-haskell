ba1012d85b6dc6a6135e4f45d10f4ee9837a8612
Renārs Kudiņš <renars.kudins@gmail.com>
2019-03-04 16:55:51 +0200

    polyWords

--- src/katas/PolyvariadicFunctions.hs
+++ src/katas/PolyvariadicFunctions.hs
@@ -1,31 +1,54 @@
+{-# LANGUAGE FlexibleInstances #-}
 module PolyvariadicFunctions(
   polyAdd,
   polyList,
   polyWords
   ) where
 
-class (Monoid a) => PolyAdder a where
-  polyAdd :: Int -> a
+class PolyAdderInt a where
+  addI :: Int -> a
 
+{-
 instance Semigroup Int where
   (<>) = (+)
 
 instance Monoid Int where
   mempty = 0
+-}
 
-instance PolyAdder Int where
-  polyAdd = id
+instance PolyAdderInt Int where
+  addI = id
 
-instance (Integral a, PolyAdder r) => PolyAdder (a -> r) where
-  polyAdd x = polyAdd . (mappend x) . fromIntegral
+instance (Integral a, PolyAdderInt r) => PolyAdderInt (a -> r) where
+  addI x = addI . (x +) . fromIntegral
+
+polyAdd :: PolyAdderInt a => a
+polyAdd = addI 0
 
 -- `polyAdd` sums its arguments, all `Int`s.
 -- polyAdd ::
+
+class PolyAdderList a where
+  addL :: [a] -> a
+
+instance PolyAdderList a where
+  addL = id
 
 -- `polyList` turns its arguments into a list, polymorphically.
 polyList :: [a] -> [a]
 polyList = error "TODO: polyList"
 
+
+class PolyAdderStr a where
+  addS :: String -> a
+
+instance PolyAdderStr String  where
+  addS = id
+
+instance PolyAdderStr r => PolyAdderStr (String -> r) where
+  addS s1 s2 = addS(s1 ++ s2)
+
+
 -- `polyWords` turns its arguments into a spaced string.
-polyWords :: String -> String
-polyWords = error "TODO: polyList"
+polyWords :: PolyAdderStr r => r
+polyWords = addS ""
