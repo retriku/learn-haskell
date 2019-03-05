16947a42edb775f1a39f2b19dd0c5ac21f4cc6bc
Renārs Kudiņš <renars.kudins@gmail.com>
2019-03-05 14:42:28 +0200

    polyList

--- src/katas/PolyvariadicFunctions.hs
+++ src/katas/PolyvariadicFunctions.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module PolyvariadicFunctions(
   polyAdd,
   polyList,
@@ -7,49 +9,32 @@ module PolyvariadicFunctions(
 
 class PolyAdderInt a where
   addI :: Int -> a
-
-{-
-instance Semigroup Int where
-  (<>) = (+)
-
-instance Monoid Int where
-  mempty = 0
--}
-
 instance PolyAdderInt Int where
   addI = id
-
 instance (Integral a, PolyAdderInt r) => PolyAdderInt (a -> r) where
   addI x = addI . (x +) . fromIntegral
 
-polyAdd :: PolyAdderInt a => a
-polyAdd = addI 0
-
--- `polyAdd` sums its arguments, all `Int`s.
--- polyAdd ::
-
-class PolyAdderList a where
-  addL :: [a] -> a
-
-instance PolyAdderList a where
-  addL = undefined
-
--- `polyList` turns its arguments into a list, polymorphically.
-polyList :: [a] -> [a]
-polyList = error "TODO: polyList"
-
-
 class PolyAdderStr a where
   addS :: String -> a
-
 instance PolyAdderStr String  where
   addS = id
-
 instance PolyAdderStr r => PolyAdderStr (String -> r) where
   addS s1 s2 = addS(s1 ++ d ++ s2)
     where d = if (null s1) then "" else " "
 
+class PolyAdderList a r | r -> a  where
+  addL :: [a] -> r
+instance PolyAdderList a [a] where
+  addL = id
+instance PolyAdderList a r => PolyAdderList a (a -> r) where
+  addL r e = addL(r ++ [e])
 
--- `polyWords` turns its arguments into a spaced string.
+
+polyAdd :: PolyAdderInt r => r
+polyAdd = addI 0
+
 polyWords :: PolyAdderStr r => r
 polyWords = addS ""
+
+polyList :: PolyAdderList a r => r
+polyList = addL []
